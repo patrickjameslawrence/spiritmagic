414696ee597828362dbe96f411d2254f325d665c
Patrick Lawrence <patrickjameslawrence@icloud.com>
2022-01-07 14:55:01 -0500

    Added most of the new block tags

--- src/main/java/com/patrickjameslawrence/spiritmagic/data/RecipeProvider.java
+++ src/main/java/com/patrickjameslawrence/spiritmagic/data/RecipeProvider.java
@@ -7,10 +7,12 @@ import com.patrickjameslawrence.spiritmagic.init.Blocks;
 
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.recipes.FinishedRecipe;
+import net.minecraft.data.recipes.RecipeBuilder;
 import net.minecraft.data.recipes.ShapedRecipeBuilder;
 import net.minecraft.data.recipes.ShapelessRecipeBuilder;
 import net.minecraft.tags.Tag;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.level.ItemLike;
 
 public class RecipeProvider extends net.minecraft.data.recipes.RecipeProvider {
@@ -29,8 +31,18 @@ public class RecipeProvider extends net.minecraft.data.recipes.RecipeProvider {
 		ShapelessRecipeBuilder.shapeless(p_126019_, 4).requires(p_126020_).group("planks")
 				.unlockedBy("has_logs", has(p_126020_)).save(p_126018_);
 	}
-	
+
 	private static void woodFromLogs(Consumer<FinishedRecipe> p_126003_, ItemLike p_126004_, ItemLike p_126005_) {
-	      ShapedRecipeBuilder.shaped(p_126004_, 3).define('#', p_126005_).pattern("##").pattern("##").group("bark").unlockedBy("has_log", has(p_126005_)).save(p_126003_);
-	   }
+		ShapedRecipeBuilder.shaped(p_126004_, 3).define('#', p_126005_).pattern("##").pattern("##").group("bark")
+				.unlockedBy("has_log", has(p_126005_)).save(p_126003_);
+	}
+
+	private static RecipeBuilder stairBuilder(ItemLike p_176711_, Ingredient p_176712_) {
+		return ShapedRecipeBuilder.shaped(p_176711_, 4).define('#', p_176712_).pattern("#  ").pattern("## ")
+				.pattern("###");
+	}
+	
+	private static RecipeBuilder slabBuilder(ItemLike p_176705_, Ingredient p_176706_) {
+		return ShapedRecipeBuilder.shaped(p_176705_, 6).define('#', p_176706_).pattern("###");
+	}
 }
--- src/main/java/com/patrickjameslawrence/spiritmagic/data/tag/BlockTagsProvider.java
+++ src/main/java/com/patrickjameslawrence/spiritmagic/data/tag/BlockTagsProvider.java
@@ -15,9 +15,17 @@ public class BlockTagsProvider extends net.minecraft.data.tags.BlockTagsProvider
 	@Override
 	protected void addTags() {
 		// TODO: Add crops and bee growables
-		this.tag(BlockTags.ROWAN_LOGS).add(Blocks.ROWAN_WOOD.get(), Blocks.STRIPPED_ROWAN_WOOD.get(), Blocks.ROWAN_LOG.get(), Blocks.STRIPPED_ROWAN_LOG.get());
+		tag(BlockTags.ROWAN_LOGS).add(Blocks.ROWAN_WOOD.get(), Blocks.STRIPPED_ROWAN_WOOD.get(), Blocks.ROWAN_LOG.get(), Blocks.STRIPPED_ROWAN_LOG.get());
 		
-		this.tag(net.minecraft.tags.BlockTags.PLANKS).add(Blocks.ROWAN_PLANKS.get());
-		this.tag(net.minecraft.tags.BlockTags.LOGS_THAT_BURN).addTags(BlockTags.ROWAN_LOGS);
+		tag(net.minecraft.tags.BlockTags.PLANKS).add(Blocks.ROWAN_PLANKS.get());
+		tag(net.minecraft.tags.BlockTags.LOGS_THAT_BURN).addTags(BlockTags.ROWAN_LOGS);
+		tag(net.minecraft.tags.BlockTags.WOODEN_BUTTONS).add(Blocks.ROWAN_BUTTON.get());
+		tag(net.minecraft.tags.BlockTags.WOODEN_DOORS).add(Blocks.ROWAN_DOOR.get());
+		tag(net.minecraft.tags.BlockTags.WOODEN_STAIRS).add(Blocks.ROWAN_STAIRS.get());
+		tag(net.minecraft.tags.BlockTags.WOODEN_SLABS).add(Blocks.ROWAN_SLAB.get());
+		tag(net.minecraft.tags.BlockTags.WOODEN_FENCES).add(Blocks.ROWAN_FENCE.get());
+		tag(net.minecraft.tags.BlockTags.WOODEN_PRESSURE_PLATES).add(Blocks.ROWAN_PRESSURE_PLATE.get());
+		tag(net.minecraft.tags.BlockTags.WOODEN_TRAPDOORS).add(Blocks.ROWAN_TRAPDOOR.get());
+		tag(net.minecraft.tags.BlockTags.LEAVES).add(Blocks.ROWAN_LEAVES.get());
 	}
 }
